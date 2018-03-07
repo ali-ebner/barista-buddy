0c5337e283117e3f3fde759f44a60f96b28a47a9
Ali Ebner <Ali@Alis-MacBook-Air.local>
2018-03-07 12:58:03 -0600

    basic conversion functions

--- conversions.js
+++ conversions.js
@@ -1,4 +1,31 @@
-var brewMethod = "Clever";
-function gramstoOz(grams){
-  return grams * 0.04;
+var from = prompt("Conversion from: ");
+var to = prompt("To: ");
+
+function convert(from,to){
+  if(from.toLowerCase()==="grams" && (to.toLowerCase()==="ounces" || to.toLowerCase()==="oz")){
+    return from + " grams = " gramstoOz(from) + " oz";
+  }
+
+}
+
+
+
+
+function gramstoOz(g){
+  return g * 0.04;
+}
+function ozToGrams(oz){
+  return oz * 28.3495;
+}
+function tbsToGrams(tbs){
+  return tbs * 14.3;
+}
+function gramsToTbs(g){
+  return g / 14.3;
+}
+function fahrenheitToCelcius(temp){
+  return (temp-32) * (5/9);
+}
+function celciusToFahrenheit(temp){
+  return temp * (9/5) + 32;
 }
